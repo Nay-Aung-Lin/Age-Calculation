f8ab2dcd23e0b5d95f90aff20fd77203811b95b7
Nicky <Theoryt8@gmail.com>
2022-08-24 14:35:17 +0630

    Age Calculation

---
+++ app.js
@@ -0,0 +1,31 @@
+const button = document.querySelector("#calc");
+const display = document.querySelector("#display");
+
+button.addEventListener("click", function () {
+  let day = Number(document.getElementById("dd").value);
+  let month = Number(document.getElementById("mm").value);
+  let year = Number(document.getElementById("yy").value);
+
+  if (day && month && year) {
+    let date = new Date();
+    let passDay = date.getDate();
+    let passMonth = date.getMonth() + 1;
+    let fullYear = date.getFullYear();
+
+    let monthArr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (passDay < day) {
+      passsDay += monthArr[passMonth - 1];
+      passMonth -= 1;
+    }
+    if (passMonth < month) {
+      passMonth += 12;
+      fullYear -= 1;
+    }
+
+    display.innerHTML = `You are pass ${fullYear - year}Years  ${
+      passMonth - month
+    }Months and ${passDay - day}Days`;
+  } else {
+    alert("Please Fill Out All Data");
+  }
+});
